6d3f876a2cbaf4d8b58c3e50b4d541cb3a903150
Rohan Dhomne <dhomnerohan@gmail.com>
2024-07-19 17:20:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,203 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+  <div> 
+    <div class='d1'>
+      <img class="i1" src="Subtract.jpg"  width="42px" height="42px"/>
+      <p >Base</p>
+
+      <ul>
+          <li ><a className='list1' href="Dashboard">Dashboard</a>
+                <img class="n1"src="Category.png"  width="24px" height="24px"/>
+          </li>
+          <li><a className='list2' href="Analytics">Analytics</a>
+                <img class="n2"src="Chart.png"  width="24px" height="24px"/>  
+          </li>
+        
+          <li><a className='list3'  href="Invoice">Invoice</a>
+                <img class="n3"src="Ticket.png"  width="24px" height="24px"/>        
+          </li>
+          <li><a className='list4'  href="Schedule">Schedule</a>
+                 <img class="n4"src="Document.png"  width="24px" height="24px"/>
+          </li>
+
+          <li><a className='list5' href="Calendar">Calendar</a>
+                 <img class="n5"src="Calendar.png"  width="24px" height="24px"/>
+          </li>
+          <li><a className='list6'  href="Messages">Messages</a>
+                  <img class="n6"src="Activity.png"  width="24px" height="24px"/></li>
+          <li><a className='list7'  href="Notification">Notification</a>
+                  <img class="n7"src="Notification.png"  width="24px" height="24px"/></li>
+          <li><a className='list8'  href="Settings">Settings</a>
+                <img class="n8"src="Setting.jpg"  width="24px" height="24px"/></li>
+      </ul>
+
+
+      
+      <img class="n9"src="cone11.png"  width="150px" height="150px"/>
+      
+      <img class="n10"src="Rectangle 20.png"  width="150px" height="150px"/>
+
+      <img class="n11"src="Vector 8.png"  width="150px" height="150px"/>
+
+      <button class="button" href="Upgrade Now">Upgrade Now</button>
+      <img class="n12"src="Log Out.png"></img>
+    </div>
+
+                  <div class='d2'>
+
+                        <h2 className='H2'>
+                        Product Analytics
+                        </h2>
+                        <img class="n13"src="btn.png"></img>
+                        <img class="n14"src="Group 396.png"></img>
+                  </div>
+
+
+                  <div class='d3'>
+
+                <button class="button2" href='product' >Product</button>
+                  <button class="button3">Customer</button>
+                  <button class="button4"> + Add Product</button>     
+                  </div>
+
+                            <div class='d4'>
+                                    <p class='p1'>Total Product</p>
+                                    <p class='p2'> 5,00,874 </p>
+                                    <p class='p3'> +1400 New Added </p>
+
+                                    <img class="img1"src="icon grup.png"></img>
+                                    <img class="img2"src="Vector 4.png"></img>
+                                    <img class="img3"src="Vector 3.png"></img>
+                            </div>
+
+
+
+                            <div class='d5'>
+                                    <p class='p1'>Total Sales</p>
+                                    <p class='p2'> 2,34,888 </p>
+                                    <p class='p3'> +1000 Sales Today </p>
+
+                                    <img class="img4"src="icon.png"></img>
+                                    <img class="img5"src="Group 399.png"></img>
+                                    <img class="img6"src="Vector 6.png"></img>
+                                    <img class="img7"src="Vector 5.png"></img>
+                            </div>
+
+
+
+
+                             <div class='table'>
+
+                              <p class='con1'> Top Selling Products </p>   
+                              
+			               <a id="l1"href="">See More</a>
+
+                                 <img class="tab1"src="table head.png"></img>
+
+
+
+                              <div class='di1'> 
+                                    
+                                     <img class="tab2"src="Position.png"></img>
+
+                                     <img class="ti1"src="image (1).png"></img>    
+
+                                     <a class="li1"href="">Blutooth Devices</a>
+
+                                     <p class='pr1'>$10</p>
+
+                                     <p class='pr2'>34,666 Piece</p>
+
+                                     <p class='pr3'>$3,46,660</p>
+                              
+                                </div>
+
+
+                              <div class='di2'>
+                              <img class="tab3"src="Position.png"></img>
+                              <img class="ti2"src="image (2).png"></img> 
+
+                              <a class="li2"href="">Airdot </a>
+                              <p class='pr1'>$15</p>
+                              <p class='pr2'>20,000 Piece</p>
+                              <p class='pr3'>$3,00,000</p>
+
+                              </div>
+
+
+                              <div class='di3'>
+                              <img class="tab3"src="Position.png"></img>
+                              <img class="ti3"src="image (3).png"></img> 
+                              <a class="li3"href="">Shoes</a>
+                              <p class='pr1'>$15</p>
+                              <p class='pr2'>15,000 Piece</p>
+                              <p class='pr3'>$1,50,000</p>
+
+                              </div>
+
+
+                              <div class='di4'>
+                                    <p class='no1'>4</p>
+                                    <img class="ti4"src="image (4).png"></img> 
+                                    <a class="li4"href="">Kids T-Shirt</a>
+                                    <p class='pr1'>$12</p>
+                                    <p class='pr2'>10,000 Piece</p>
+                                    <p class='pr3'>$1,20,000</p>
+                              </div>
+
+
+                              <div class='di5'>
+                                    <p class='no2'>5</p>
+                                    <img class="ti5"src="image (5).png"></img> 
+                                    <a class="li5"href="">Smart Watch</a>
+
+                                    <p class='pr1'>$12</p>
+                                    <p class='pr2'>10,000 Piece</p>
+                                    <p class='pr3'>$1,20,000</p>
+                              </div>
+
+
+                              <div class='di6'>
+                                  <p class='no3'>6</p> 
+                                  <img class="ti6"src="image (6).png"></img>  
+                                  <a class="li6"href="">Girls Top</a>
+                                  <p class='pr1'>$12</p>
+                                  <p class='pr2'>10,000 Piece</p>
+                                  <p class='pr3'>$1,20,000</p>
+                              </div>
+                            
+                             </div>
+
+                           <div class='graph'>
+
+                              <div class ='container'>
+                             <p class='text1' >Product Add  by Month</p>
+
+                              <img class="pro"src="bar1.png"></img>
+                              
+                              </div>
+                           
+                                  <div class ='container1'>
+                                  <p class='text2'>Product Sales Analytics</p>
+                                  <img class="pro2"src="circle.png"></img>
+
+
+
+                                  
+
+                                  </div>  
+                           
+                              </div>  
+
+
+
+</div>   
+
+  );
+}
+
+export default App;
---
+++ src/Bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Bar.css';
+
+
+const Bar=()=>{
+
+    return(
+
+        <h1></h1>
+    );
+};
+
+
+export default Bar;
